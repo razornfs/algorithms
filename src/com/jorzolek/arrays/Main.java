9ef5057c9ce4631f844b151d4b81d973e5fc8b54
Bonkers <kubaorz1@gmail.com>
2019-04-13 11:15:01 +0200

    test2

--- src/com/jorzolek/arrays/Main.java
+++ src/com/jorzolek/arrays/Main.java
@@ -3,7 +3,13 @@ package com.jorzolek.arrays;
 public class Main {
 
     public static void main(String[] args) {
-        MyGenericArray<String> myGenericArray = new MyGenericArray<>(new String[]{"aa", "bb", "cc"});
-        myGenericArray.print();
+/*        MyGenericArray<String> myGenericArray = new MyGenericArray<>(new String[]{"aa", "bb", "cc"});
+        myGenericArray.print();*/
+        MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
+        myArray.print();
+        System.out.println(myArray.shift());
+        myArray.print();
+        myArray.unshift(1);
+        myArray.print();
     }
 }
